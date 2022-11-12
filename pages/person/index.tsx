580eb012b0b188f67afd8f8d473f57345395a5dd
ffpaiki <74241058+ffpaiki@users.noreply.github.com>
2022-11-13 06:34:26 +0900

    Create new template

--- pages/person/index.tsx
+++ pages/person/index.tsx
@@ -1,9 +1,7 @@
-export default function PersonDetail() {
+export default function Home() {
   return (
-    <div> 
-      <main>
-        <h1>Person Detail</h1>
-      </main>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
   )
 }
